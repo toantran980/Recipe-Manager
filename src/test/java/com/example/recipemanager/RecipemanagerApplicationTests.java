b49dbd5161728f2b4712e9c7fc6f0ee2d778e4d8
toantran980 <jokemaking90@gmail.com>
2026-07-25 18:09:17 -0700

    add test

--- src/test/java/com/example/recipemanager/RecipemanagerApplicationTests.java
+++ src/test/java/com/example/recipemanager/RecipemanagerApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.recipemanager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"jwt.secret=test-secret-for-CI-with-32-chars-minimum!!"})
 class RecipemanagerApplicationTests {
 
 	@Test
